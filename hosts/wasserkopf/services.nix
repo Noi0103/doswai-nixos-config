6fddf4abdda3e3040d01c3001523ddd1c4fd0408
Noi0103 <noi0103@proton.me>
2025-08-25 00:27:14 +0200

    make preparations for new domains and cryptpad

--- hosts/wasserkopf/services.nix
+++ hosts/wasserkopf/services.nix
@@ -7,7 +7,7 @@
 }:
 let
   maxUploadSize = "5G";
-  domain = "dummkopf.crabdance.com";
+  domain = "crabdance.com";
   statefulDir = "/mnt/operation";
   backupDir = "/mnt/backup";
 in
@@ -25,7 +25,7 @@ in
 
     clientMaxBodySize = "${maxUploadSize}"; # when uploading large files on nextcloud
 
-    virtualHosts."${domain}" = {
+    virtualHosts."dummkopf.${domain}" = {
       forceSSL = true;
       enableACME = true;
       root = "${inputs.doswai-frontend.packages."x86_64-linux".frontend}/";
@@ -36,11 +36,7 @@ in
       enableACME = true;
       locations."/".proxyPass = "http://127.0.0.1:20001";
     };
-    #virtualHosts."www.${domain}" = {
-    #  forceSSL = false;
-    #  enableACME = false;
-    #  root = "/var/www/${domain}";
-    #};
+
     #virtualHosts."${config.services.nextcloud.hostName}" = {
     #  forceSSL = true;
     #  enableACME = true;
@@ -50,6 +46,11 @@ in
     #  forceSSL = true;
     #  enableACME = true;
     #  locations."/".proxyPass = "http://127.0.0.1:8080";
+    #};
+    #virtualHosts."${config.services.cryptpad.settings.httpSafeOrigin}" = {
+    #  forceSSL = true;
+    #  enableACME = true;
+    #  locations."/".proxyPass = "http://127.0.0.1:3000";
     #};
   };
   networking.firewall.allowedTCPPorts = [
@@ -68,7 +69,7 @@ in
     enable = false;
     package = pkgs.nextcloud31;
     https = true;
-    hostName = "cloud.${domain}";
+    hostName = "nextcloud.wasserkopf.${domain}";
     settings = {
       trusted_domain = [ "${config.services.nextcloud.hostName}" ];
       trusted_proxies = [ "127.0.0.1" ];
@@ -99,7 +100,7 @@ in
     statePath = "${statefulDir}/gitlab";
     initialRootEmail = "alice@local.host";
     initialRootPasswordFile = config.sops.secrets."gitlab-admin-init".path;
-    host = "git.${domain}";
+    host = "gitlab.wasserkopf.${domain}";
     databaseCreateLocally = true;
     # https://docs.gitlab.com/administration/backup_restore/backup_gitlab/
     backup = {
@@ -108,5 +109,17 @@ in
       path = /mnt/backup;
       keepTime = 24 * 7;
     };
+  };
+
+  ## cryptpad
+  services.cryptpad = {
+    enable = false;
+    settings = {
+      httpPort = 3000;
+      httpUnsafeOrigin = "http://127.0.0.1";
+      httpSafeOrigin = "https://cryptpad.wasserkopf.${domain}";
+      adminKeys = [ ];
+    };
+    # TODO missing backups
   };
 }
